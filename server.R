bcfb25170d3ed1315f9f3a6478536c45163e2a58
chazzy1 <chazzy1@gmail.com>
2018-10-28 00:07:47 -0400

    tabpanel

--- server.R
+++ server.R
@@ -2,8 +2,20 @@ library(shiny)
 library(leaflet)
 library(leaflet.extras)
 
+noisedata <-
+  read.csv(file = "data//311_Noise_Complaints_1month_manhattan.csv",
+           header = TRUE,
+           sep = ",")
+
 
 shinyServer(function(input, output, session) {
+  values <- reactiveValues()
+  values$isNoisedataLoaded <- FALSE
+  
+  output$summary <- renderPrint({
+    print("1234")
+  })
+  
   output$myMap <- renderLeaflet({
     leaflet() %>%
       addTiles(group = "Default") %>%
@@ -13,55 +25,72 @@ shinyServer(function(input, output, session) {
         baseGroups = c("Default", "Satellite Maptile"),
         options = layersControlOptions(collapsed = FALSE)
       )
+    
   })
   
-  observe({
-    withProgress(message = 'Please wait...',
-                 value = 0 / 4, {
-                   
-                   incProgress(1 / 4, detail = "reading data")
-                   
-                   noisedata <-
-                     read.csv(file = "data//311_Noise_Complaints_1month_manhattan.csv",
-                              header = TRUE,
-                              sep = ",")
-
-                   incProgress(1 / 4, detail = "preparing data")
-                   
-                   myData <- noisedata %>%
-                     select(Created.Date, Descriptor,Borough, Latitude, Longitude) %>%
-                     drop_na()
-                   
-                   incProgress(1 / 4, detail = "rendering map")
-
-                   leafletProxy("myMap", data = myData) %>%
-                     fitBounds(~ min(Longitude), ~ min(Latitude), ~ max(Longitude), ~
-                                 max(Latitude)) %>%
-                     addHeatmap(
-                       lng = ~ Longitude,
-                       lat = ~ Latitude,
-                       group = "HeatMap1",
-                       blur = 20,
-                       max = 0.01,
-                       radius = 15
-                     ) %>%
-                     addHeatmap(
-                       lng = ~ Longitude,
-                       lat = ~ Latitude,
-                       group = "HeatMap2",
-                       blur = 10,
-                       max = 0.01,
-                       radius = 1
-                     ) %>%                   
-                     addLayersControl(
-                       baseGroups = c("Default", "Satellite Maptile"),
-                       overlayGroups = c("HeatMap1", "HeatMap2"),
-                       options = layersControlOptions(collapsed = FALSE)
-                     )
-                   
-                   incProgress(1 / 4)
-                   
-                 })
+  observeEvent(input$submenu, {
+    if (input$submenu == "heatmap" && !values$isNoisedataLoaded) {
+      values$isNoisedataLoaded = TRUE
+      withProgress(message = 'Please wait...',
+                   value = 0 / 4, {
+                     incProgress(1 / 4, detail = "reading data")
+                     
+                     
+                     incProgress(1 / 4, detail = "preparing data")
+                     
+                     myData <- noisedata %>%
+                       select(Created.Date, Descriptor, Borough, Latitude, Longitude) %>%
+                       drop_na()
+                     
+                     incProgress(1 / 4, detail = "rendering map")
+                     
+                     
+                     
+                     leafletProxy("myMap",
+                                  data = subset(myData, Descriptor == "Loud Music/Party")) %>%
+                       fitBounds(~ min(Longitude),
+                                 ~ min(Latitude),
+                                 ~ max(Longitude),
+                                 ~
+                                   max(Latitude)) %>%
+                       addHeatmap(
+                         lng = ~ Longitude,
+                         lat = ~ Latitude,
+                         group = "HeatMap2",
+                         blur = 10,
+                         max = 0.01,
+                         radius = 1
+                       )
+                     
+                     
+                     leafletProxy("myMap", data = myData) %>%
+                       fitBounds(~ min(Longitude),
+                                 ~ min(Latitude),
+                                 ~ max(Longitude),
+                                 ~
+                                   max(Latitude)) %>%
+                       addHeatmap(
+                         lng = ~ Longitude,
+                         lat = ~ Latitude,
+                         group = "HeatMap1",
+                         blur = 20,
+                         max = 0.01,
+                         radius = 15
+                       ) %>%
+                       addLayersControl(
+                         baseGroups = c("Default", "Satellite Maptile"),
+                         overlayGroups = c("HeatMap1", "HeatMap2"),
+                         options = layersControlOptions(collapsed = FALSE)
+                       )
+                     
+                     
+                     incProgress(1 / 4)
+                     
+                   })
+      
+      
+    }
+    
   })
   
   
--- ui.R
+++ ui.R
@@ -3,19 +3,38 @@ library(shinydashboard)
 library(leaflet)
 library(leaflet.extras)
 
-# Define UI for application that plots random distributions 
-shinyUI(
-  dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody(
-      
-      
-      leafletOutput("myMap")
-      
+# Define UI for application that plots random distributions
+
+
+
+
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(id="submenu",
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Heatmap", icon = icon("th"), tabName = "heatmap",
+             badgeLabel = "new", badgeColor = "green")
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            {
+              verbatimTextOutput("summary")
+            }
+    ),
+    
+    tabItem(tabName = "heatmap",
+            leafletOutput("myMap")
+            
     )
   )
-  
-  
-  
+)
+
+
+dashboardPage(
+  dashboardHeader(title = "Simple tabs"),
+  sidebar,
+  body
 )
